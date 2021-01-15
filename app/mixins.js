3c6b70d00153fde642814b5268b795de4577839c
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-01-15 09:56:43 +0700

    # Add mixins file

---
+++ app/mixins.js
@@ -0,0 +1,12 @@
+const second = array => (array && array.length) ? array[1] : undefined;
+const third = array => (array && array.length) ? array[2] : undefined;
+const fourth = array => (array && array.length) ? array[3] : undefined;
+
+module.exports = {
+    second,
+    third,
+    fourth
+}
+
+
+
