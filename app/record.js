513031f1b2777ab0aa92b1ea02c16cbacd6560f3
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-01-16 01:59:29 +0700

    # Add methods comments

--- app/record.js
+++ app/record.js
@@ -13,16 +13,35 @@ class Record {
         this.url = url;
     }
 
+    /***
+     *
+     * @returns {*[]}
+     */
     toArray = () =>
         _.chain(this)
             .map((v, i) => typeof v !== 'function' ? [i, v] : null)
             .filter(v => !_.isNull(v))
             .value()
 
+    /***
+     *
+     * @returns {boolean}
+     */
     empty = () =>this.values().every(v => !v);
+    /***
+     *
+     * @returns {*[]}
+     */
     keys = () => this.toArray().map(_.first);
+    /***
+     *
+     * @returns {*[]}
+     */
     values = () => this.toArray().map(second);
-
+    /***
+     *
+     * @returns {boolean}
+     */
     active = () => this.status.includes('Действующая');
 }
 
