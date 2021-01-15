3b6bf068b91582d8bf1e75145aef2a0ae283786b
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-01-15 16:50:42 +0700

    # add function five

--- app/mixins.js
+++ app/mixins.js
@@ -1,11 +1,13 @@
 const second = array => (array && array.length) ? array[1] : undefined;
 const third = array => (array && array.length) ? array[2] : undefined;
 const fourth = array => (array && array.length) ? array[3] : undefined;
+const five = array => (array && array.length) ? array[4] : undefined;
 
 module.exports = {
     second,
     third,
-    fourth
+    fourth,
+    five
 }
 
 
