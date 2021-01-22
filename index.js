013927dc91c49a890b2e3d0f50a94b69d41d267f
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-01-22 22:03:47 +0700

    # Add Запись загруженных и распарсенных данных в БД

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const {timer} = require('rxjs');
 const _ = require('lodash');
 const {launchDriver} = require('./app/selenium');
 const {Parser} = require('./app/parser');
-const {groupBy, traverse, formatBeforeSave, collectInformation} = require('./app/utils');
+const {toRecord} = require('./app/utils');
 const {File} = require('./app/files');
+const {second} = require('./app/mixins');
+const {DbInstance} = require('./app/db');
 
 (async () => {
     /***
@@ -12,8 +14,10 @@ const {File} = require('./app/files');
      *
      *  Данные для записи в БД
      */
+        // const source = [];
+    const records = [];
 
-    // File.deleteStore();
+    File.deleteStore();
 
     /***
      *  Вызывается на каждой итерацц перехода по ссылке
@@ -22,90 +26,35 @@ const {File} = require('./app/files');
     const action = ({iteration, url, pageSource}) => {
         const parser = new Parser(pageSource);
         const html = parser.getHtml('.table.reee_table');
-        const record = collectInformation(parser, {iteration, url});
-        const information = record.toArray();
+        const record = toRecord(parser, {iteration, url});
+        const recordData = record.values();
 
+        records.push(record.toSaveDb());
+        // source.push(html);
 
         File.save(File.SOURCE, `iteration: ${iteration}|<table>${html}</table>|${url}`);
-        File.save(File.INFO, information.join('|'));
+        File.save(File.INFO, recordData.join('|'));
     };
 
     await launchDriver(action);
 
     /***
-     *  Запись в таблицы БД
+     * Actions after
      *
      */
-
-    /**
-     *  После сбора инфы, формируем файлы
-     */
-    // const subscription = timer(5000)
-    //     .subscribe(
-    //         () => {
-    //             // const info = File.parseInfoFile();
-    //             // const info = File.parseInfoFile().map(v => v.toArray())
-    //             //
-    //             // const byInnKpp = groupBy(info, ['inn', 'kpp']);
-    //             // /***
-    //             //  *  Save
-    //             //  */
-    //             // const byInnKppOnce = _.chain(byInnKpp)
-    //             //     .map(v => _.map(v, k => k))
-    //             //     .flatten()
-    //             //     .filter(v => v.length < 2)
-    //             //     .flatten()
-    //             //     .value()
-    //             // ;
-    //             // File.save(File.GROUP_BY_INN_KPP_ONCE, formatBeforeSave(byInnKppOnce).join('\n'));
-    //             // // console.log(convertToSave(byInnKppOnce));
-    //             //
-    //             // /***
-    //             //  *  Save
-    //             //  */
-    //             // const byInnKppMore = _.chain(byInnKpp)
-    //             //     .map(v => _.map(v, k => k))
-    //             //     .flatten()
-    //             //     .filter(v => v.length > 1)
-    //             //     .flatten()
-    //             //     .value()
-    //             // ;
-    //             // File.save(File.GROUP_BY_INN_KPP_MORE, formatBeforeSave(byInnKppMore).join('\n'));
-    //             // // console.log(convertToSave(byInnKppMore));
-    //             //
-    //             // /***
-    //             //  *  Доп. группировка по дубликатам
-    //             //  *  Ищем одинаковые элементы
-    //             //  *
-    //             //  */
-    //             // const grouped = groupBy(byInnKppMore, ['inn', 'kpp', 'title', 'codes']);
-    //             //
-    //             // /***
-    //             //  *  Записи, в которых одинаковые 'inn', 'kpp', 'title', 'codes'
-    //             //  *
-    //             //  *  Берём только первые, ибо сгруппированыые записи одинаковые
-    //             //  */
-    //             // const same = traverse(grouped).filter(v => v.length > 1)/*.flatMap(v => v)*/.map(_.first);
-    //             // const difference = traverse(grouped).filter(v => v.length < 2).flatMap(v => v);
-    //             //
-    //             // File.save(File.SAME, formatBeforeSave(same).join('\n'));
-    //             // File.save(File.DIFFERENCE, formatBeforeSave(difference).join('\n'));
-    //             //
-    //             // File.save(`group_by_inn_kpp_once+same.csv`, formatBeforeSave(_.concat(byInnKppOnce, same)));
-    //             //
-    //             // console.log(
-    //             //     _.concat(byInnKppOnce, same).length,
-    //             //     byInnKppOnce.length,
-    //             //     byInnKppMore.length,
-    //             //     same.length,
-    //             //     difference.length
-    //             // )
-    //
-    //
-    //         },
-    //         error => console.error(error),
-    //         () => subscription.unsubscribe()
-    //     );
-
-
+    const subscription = timer(5000)
+        .subscribe(async () => {
+                /***
+                 * Save to Db
+                 */
+                await DbInstance.insertRecords(records);
+                // console.log(records);
+            },
+            e => {
+                console.log(e);
+            },
+            () => {
+                subscription.unsubscribe();
+            }
+        )
 })();
